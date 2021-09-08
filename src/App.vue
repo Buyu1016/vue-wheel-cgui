d1a82ef18116d33440e3eeeed98492065106ea4e
CodeGorgeous <2460481461@qq.com>
2021-09-08 14:25:26 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@ export default {
   data() {
     return {
       url: [
-        'https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fpic4.zhimg.com%2Fv2-cef492aa2c7be6261b22d60a3e806789_r.jpg&refer=http%3A%2F%2Fpic4.zhimg.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1632405993&t=adc51a5ac55a0b0a7bcd11033b7e6a45',
+        'https://img1.baidu.com/it/u=2344733995,569157975&fm=26&fmt=auto&gp=0.jpg',
       ]
     }
   }
--- src/lib/CgUI/directives/magnifier.js
+++ src/lib/CgUI/directives/magnifier.js
@@ -7,24 +7,35 @@ const magnifier = Vue.directive('magnifier', {
         const oImage = el.children[0]
         const oMag = el.children[1]
         const oMagImage = oMag.children[0]
-        el.addEventListener('mouseover', () => {
-            oMag.style.display = 'inline-block'
-            document.addEventListener('mousemove', documentMouseMove, false)
-        }, false)
-        el.addEventListener('mouseout', (e) => {
-            oMag.style.display = 'none'
-            document.removeEventListener('mousemove', documentMouseMove)
-        }, false)
+        el.addEventListener('mouseenter', elMouseEnter, false)
+        el.addEventListener('mouseleave', elMouseLeave, false)
 
         function documentMouseMove(e) {
             const elRect = el.getBoundingClientRect()
-            console.log(e.target)
             const x = e.clientX - parseInt(elRect['left']) - oMag.getBoundingClientRect()['width'] / 2
             const y = e.clientY - parseInt(elRect['top']) - oMag.getBoundingClientRect()['height'] / 2
+            if (e.clientX < elRect['left'] || e.clientX > elRect['left'] + elRect['width']) {
+                elMouseLeave()
+                return
+            }
+            if (e.clientY < elRect['top'] || e.clientY > elRect['top'] + elRect['height']) {
+                elMouseLeave()
+                return
+            }
             oMag.style.left = x + 'px'
             oMag.style.top = y + 'px'
             oMagImage.style.left = -x + 'px'
             oMagImage.style.top = -y + 'px'
+        }
+        
+        function elMouseEnter() {
+            oMag.style.display = 'inline-block'
+            document.addEventListener('mousemove', documentMouseMove, false)
+        }
+
+        function elMouseLeave() {
+            oMag.style.display = 'none'
+            document.removeEventListener('mousemove', documentMouseMove, false)
         }
     }
 })
